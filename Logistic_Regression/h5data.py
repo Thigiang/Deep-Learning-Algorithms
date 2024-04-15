0bc502c8bb688b8cddf33584196ca985d9ab3d4c
Thi Giang <tuyetgiangst93@gmail.com>
2024-04-15 14:06:06 -0700

    adjust the dataset path

--- Logistic_Regression/h5data.py
+++ Logistic_Regression/h5data.py
@@ -1,11 +1,17 @@
 import numpy as np
 import h5py
 def load_dataset():
-    train_dataset = h5py.File('train_catvnoncat.h5', "r")
+    path = "/Users/gabati/Documents/GitHub/"
+    file_name_train, file_name_test = 'train_catvnoncat.h5', 'test_catvnoncat.h5'
+    file_path_train = path +"Datasets/"+file_name_train
+    file_path_test = path + "Datasets/"+ file_name_train
+    print(file_path_train)
+    
+    train_dataset = h5py.File(file_path_train, "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:])
     train_set_y_orig = np.array(train_dataset["train_set_y"][:])
     
-    test_dataset = h5py.File('test_catvnoncat.h5', "r")
+    test_dataset = h5py.File(file_path_test, "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:])
     test_set_y_orig = np.array(test_dataset["test_set_y"][:])
     
