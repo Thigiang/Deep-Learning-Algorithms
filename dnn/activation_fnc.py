812ec44b5f7cd46aa5022f7ae56ef6d23698c34b
Thi Giang <tuyetgiangst93@gmail.com>
2024-04-19 13:44:24 -0700

    commenting

--- dnn/activation_fnc.py
+++ dnn/activation_fnc.py
@@ -1,11 +1,11 @@
 """
-This file create helper functions (activation functions and its derivatives) to support the dnn algorithm (mainly in backward propagation step)
+This file create helper functions (activation functions and the backward functions to calculate dZ given dA and Z)
 
 This file provide algorithms to compute different activation functions that used in deep neural networks:
-    -- Sigmoid function: s(z) = 1/(1+e^(-z)) and its derivative (called sigmoid_backward function)
-    -- tanh funtion: tanh(z) = (e^z - e^(-z))/(e^z + e^(-z)) and its derivative (called tanh_backward function)
-    -- ReLU function: relu(z) = max(0,z) and its derivative (called relu_backward)
-    -- Leaky ReLU function: lrelu(z) = max(0.01z, z) and its derivative (called Leaky_relu_backward)
+    -- Sigmoid function: s(z) = 1/(1+e^(-z)) and sigmoid_backward function: calculating dZ = dA * s'(z)=dA*ds= dA*s(z)*(1-s(z))
+    -- tanh funtion: tanh(z) = (e^z - e^(-z))/(e^z + e^(-z)) and tanh_backward function: dZ = dA*dt=dA*(1-t^2)
+    -- ReLU function: relu(z) = max(0,z) and relu_backward: dZ = dA*dr where dr is the derivative of relu function: dr=1 if z>00 and dr=0 if z<0
+    -- Leaky ReLU function: lrelu(z) = max(0.01z, z) and leaky_relu_backward: dZ = dA*dlr where dlr is the derivative of leaky relu function: dlr = 0.01 if z<0 and dlr=1 if z>=0
 
 """
 import numpy as np
